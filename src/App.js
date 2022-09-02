5361188ea9a3f8c9edda97e9546fc784730ec590
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-09-02 16:31:34 +0200

    first run

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
-}
+const App = () => {
+  return <div>Hello World</div>;
+};
 
 export default App;

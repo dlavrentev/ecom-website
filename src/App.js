2dc4253d9f31ab5d1af621dd937d5acb50a7d795
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-09-06 10:01:50 +0200

    updated all dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./categories.styles.scss";
-import CategoryItem from "./components/category-item/ category-item.component";
+import Directory from "./components/directory/directory.component";
+import React from "react";
 
 const App = () => {
   const categories = [
@@ -29,9 +29,7 @@ const App = () => {
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
-  return (
-    
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
